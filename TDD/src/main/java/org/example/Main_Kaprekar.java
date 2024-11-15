4d8046174eed45ac6414de7f7eda56ecb2364b24
AdolfoGayoso <a.gayoso02@ufromail.cl>
2024-11-15 11:16:26 -0300

    Setup de los test
    setup de los test con sus metodos vacios

---
+++ TDD/src/main/java/org/example/Main_Kaprekar.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Main_Kaprekar {
+    public static void main(String[] args) {
+
+    }
+
+    public static int kaprekarOp(int num) {
+        return 0;
+    }
+
+    public static int itKaprekar(int num) {
+        return 0;
+    }
+
+}
